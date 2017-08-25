6d0b233f60e0cdd29dba852aadf215010c97e737
FlyStorm <ggloading@gmail.com>
2017-08-25 18:37:44 +0800

    a

---
+++ app/src/main/java/com/fi/activity/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.fi.activity;
+
+/**
+ * 创建者     yangyanfei
+ * 创建时间   2017/8/25 0025 18:33
+ * 作用	      ${TODO}
+ * <p/>
+ * 版本       $$Rev$$
+ * 更新者     $$Author$$
+ * 更新时间   $$Date$$
+ * 更新描述   ${TODO}
+ */
+public class HelloWorld {
+
+}
